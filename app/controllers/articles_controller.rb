95ebd3f2b848e912b5ceea38cf2c18e1b12d9962
antoniotto71git config --global user.email antoniotto71@yahoo.itssh-keygen -t rsa -C antoniotto71@yahoo.itcat ~/.ssh/id_rsa.pubgit config --global user.name antoniotto71 <antoniotto71@yahoo.it>
2019-04-27 20:10:04 +0200

    creata pagina creazione articoli e pagina per mostrarli

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,12 +5,22 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
+    if @article.save
+      flash[:notice] = "Article was succesfully created"
+    redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
 
-  def article_params
-    params.require(:article).permit(:title, :description)
+  def show
+    @article = Article.find(params[:id])
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
 end
